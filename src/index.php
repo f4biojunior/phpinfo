cfb93b26d6a9a0c2e3086d7a1bf4b42518a08900
tchewagner <47128789+tchewagner@users.noreply.github.com>
2019-02-01 12:29:02 -0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,13 @@
 <?php
 $msg="Ola Mundo do Tiago 5 - depois do if dentro do jenkins, quero ver!";
 echo $msg;
+echo '\n';
+
+echo gethostname();
+echo '\n';
+echo '\n';
+
+$date = date('Y-m-d H:i:s:u');
+echo $date;
+
 ?>
