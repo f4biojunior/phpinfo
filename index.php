87cc48ed1e726dca6c771680b88107c4026632ed
tchewagner <47128789+tchewagner@users.noreply.github.com>
2019-02-21 12:29:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-$msg="Ola Mundo do Tiago 5 - depois do if dentro do jenkins, quero ver!";
+$msg="Ola Mundo do Tiago 5 - depois do if dentro do jenkins, quero ver - agora com BR! </br>";
 echo $msg;
-echo '\n';
+echo '</br>';
 echo gethostname();
-echo '\n';
-echo '\n';
+echo '</br></br>';
 $date = date('Y-m-d H:i:s:u');
 echo $date;
-
+echo '</br></br>';
 phpinfo();
 ?>
