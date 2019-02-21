9d6efb5ee9bef478aeb00f4f9f376db36fbb17c0
tchewagner <47128789+tchewagner@users.noreply.github.com>
2019-02-21 12:24:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$msg="Ola Mundo do Tiago 5 - depois do if dentro do jenkins, quero ver!";
+echo $msg;
+echo '\n';
+echo gethostname();
+echo '\n';
+echo '\n';
+$date = date('Y-m-d H:i:s:u');
+echo $date;
+
+phpinfo();
+?>
