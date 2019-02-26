d006048ff301a98e50cdd595ad1a5511bd1cc6db
tchewagner <47128789+tchewagner@users.noreply.github.com>
2019-02-26 11:20:45 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,13 +1,13 @@
 <?php
-$msg="Ola Mundo do Tiago 5 - depois do if dentro do jenkins, quero ver!";
+$msg="Ola Mundo do Tiago 5 - depois do if dentro do jenkins, quero ver - agora com BR! </br></br>";
 echo $msg;
-echo '\n';
-
+$msg2="Openshift Ailos - 22 de Fevereiro </br>";
+echo $msg2;
+echo '</br>';
 echo gethostname();
-echo '\n';
-echo '\n';
-
+echo '</br></br>';
 $date = date('Y-m-d H:i:s:u');
 echo $date;
-
+echo '</br></br>';
+phpinfo();
 ?>
